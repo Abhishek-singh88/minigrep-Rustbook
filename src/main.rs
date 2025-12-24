6c3536e74f87baa578bd85e9d89baba7c007dd34
Abhishek Singh <abhisheksingh4928@gmail.com>
2025-12-24 17:50:28 +0530

    project 2 completed

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use std::env;
-use std::fs;
-use dotenv::dotenv;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
-    dotenv().ok();
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    let file_path = env::var("file_path").expect("FILE_PATH not set in .env");
-
-    println!("In file {file_path}");
-
-    let contents = fs::read_to_string(&file_path)
-        .expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
 }
